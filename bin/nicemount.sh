70ebf0f0447eab53d2046d5589e22375eb4909a3
Piraty <piraty1@inbox.ru>
2020-07-01 15:17:47 +0200

    bin/nicemount.sh: pretty-print

--- bin/nicemount.sh
+++ bin/nicemount.sh
@@ -1,5 +1,8 @@
-# -------------------------------------------------------------------
+#!/bin/sh
 # nice mount (http://catonmat.net/blog/another-ten-one-liners-from-commandlinefu-explained)
 # displays mounted drive information in a nicely formatted manner
-# -------------------------------------------------------------------
-nicemount() { (echo "DEVICE PATH TYPE FLAGS" && mount | awk '$2="";1') | column -t ; }
+nicemount() {
+	(
+		echo "DEVICE PATH TYPE FLAGS" && mount | awk '$2="";1'
+	) | column -t
+}
